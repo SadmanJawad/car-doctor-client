c7839fade389b705a5a0ad4062ecfaf044d15a8f
SadmanJawad <jawadnabil977@gmail.com>
2023-05-11 01:56:53 +0600

    Style Banner Component with gradient overlay

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import router from './routes/Routes';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <div className='max-w-7xl mx-auto'>
+ <div className='max-w-6xl mx-auto'>
    <React.StrictMode>
   <RouterProvider router={router} />
   </React.StrictMode>,
