1b75c917f4584970aea850da1dbf6aff3dffeb7a
SadmanJawad <jawadnabil977@gmail.com>
2023-05-14 12:56:07 +0600

    Save order on the order collection using Post Method

--- src/pages/Shared/Footer/Navbar/Navbar.jsx
+++ src/pages/Shared/Footer/Navbar/Navbar.jsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import logo from "../../../../assets/logo.svg";
+import { useContext } from "react";
+import { AuthContext } from "../../../../providers/AuthProvider";
 
 const Navbar = () => {
+
+  const {user,logOut} = useContext(AuthContext);
+  const handleLogout = () => {
+      logOut()
+      .then()
+      .catch(error => console.log(error))
+  }
+
   const navItems = (
     <>
       <li>
@@ -11,6 +21,9 @@ const Navbar = () => {
       <li>
         <Link to='about'>About</Link>
       </li>
+      {user?.email? <li><button onClick={handleLogout}>Log Out</button></li> :
+      <li><Link to='login'>Login</Link></li>  
+    }
     </>
   );
 
