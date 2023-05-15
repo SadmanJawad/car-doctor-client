d25a40dea7a6ad5b2903e573a08ff1615dc792a8
SadmanJawad <jawadnabil977@gmail.com>
2023-05-15 15:11:29 +0600

    update booking using patch

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -2,6 +2,8 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
 
+
+
 const Bookings = () => {
   const { user } = useContext(AuthContext);
   const [bookings, setBookings] = useState([]);
@@ -11,7 +13,49 @@ const Bookings = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => setBookings(data));
-  }, []);
+  }, [url]);
+
+
+  const handleDelete = id => {
+    const proceed = confirm('Are you sure you want to delete')
+    if(proceed){
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+         if(data.deletedCount > 0){
+            alert('Deleted Successfully');
+            const remaining = bookings.filter(booking => booking._id !== id)
+            setBookings(remaining)
+        }
+        })
+    }
+  }  
+
+  const handleBookingConfirm = id => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'content-type' : 'application/json',
+        },
+        body: JSON.stringify({status: 'confirm'})
+        
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        if(data.modifiedCount > 0){
+            // update state
+            const remaining = bookings.filter(booking => booking._id !== id)
+            const updated = bookings.find(booking => booking._id === id);
+            updated.status = 'confirm'
+            const newBookings = [updated, ...remaining]
+            setBookings(newBookings)
+        }
+    })
+  }
 
   return (
     <div>
@@ -38,6 +82,8 @@ const Bookings = () => {
                     bookings.map(booking => <BookingRow
                     key={booking._id}
                     booking={booking}
+                    handleDelete={handleDelete}
+                    handleBookingConfirm={handleBookingConfirm}
                     ></BookingRow>)
                 }
           </tbody>
