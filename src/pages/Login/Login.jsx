c9b0a0c0cf2b3532a9d1ea59b32af3c9bf7a9560
SadmanJawad <jawadnabil977@gmail.com>
2023-05-16 00:54:28 +0600

    Create JWT Token on server, Get jwt token on client side

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
@@ -8,21 +8,43 @@ const Login = () => {
 
    const {signIn} = useContext(AuthContext)
     const location = useLocation();
+    const navigate = useNavigate();
+
 
     const from = location.state?.from?.pathname || '/';
-    
+
 
     const handleLogin = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(name, email, password)
+        console.log( email, password)
 
         signIn(email, password)
         .then(result => {
             const user = result.user;
-            console.log(user);
+            const loggedUser = {
+                email: user.email
+            }
+            console.log(loggedUser);
+            
+            fetch('http://localhost:5000/jwt', {
+                method: 'POST',
+                headers: {
+                    'content-type' : 'application/json'
+                },
+                body: JSON.stringify(loggedUser)
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log('jwt response', data);
+                
+                //! WARNING : Local storage is not the best (2nd best place) tp store access token
+                localStorage.setItem('car-access-token', data.token)
+                navigate(from, {replace: true})
+            })
+
         })
         .catch(error => console.log(error))
         
@@ -48,7 +70,7 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
