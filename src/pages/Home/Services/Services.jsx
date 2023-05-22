8c418427eefed51a7fd72494446e22bc68e1f342
SadmanJawad <jawadnabil977@gmail.com>
2023-05-23 02:10:44 +0600

    price range added 100 to 300 same prev commit

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -17,11 +17,13 @@ const Services = () => {
                 <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
                 <h2 className="text-5xl">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
+             {/* sorting */}
                 <button
                 className="btn btn-primary mt-4"
                 onClick={() => setAscending(!ascending)}
                 >{ascending ? 'Price High to Low' : 'Price Low to High'}</button>
             </div>
+
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
                     services.map(service => <ServiceCard
